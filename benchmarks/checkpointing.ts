3947fdfcc9d596cd2db6c9ac0dca35f35b7455aa
Ryan Ghods <ryan@ryanio.com>
2020-08-20 19:28:45 -0700

    add benchmark.js and github-action-benchmark

--- benchmarks/checkpointing.ts
+++ benchmarks/checkpointing.ts
@@ -1,54 +1,20 @@
 import { pseudoRandomBytes } from 'crypto'
 import { CheckpointTrie } from '../dist'
 
-const iterations = 5000
-const samples = 5
+export const iterTest = async (numOfIter: number) => {
+  const keys: Buffer[] = []
+  const vals: Buffer[] = []
 
-const iterTest = async (numOfIter: number): Promise<Array<number>> => {
-  return new Promise(async (resolve) => {
-    let vals = [] as any
-    let keys = [] as any
-
-    for (let i = 0; i <= numOfIter; i++) {
-      vals.push(pseudoRandomBytes(32))
-      keys.push(pseudoRandomBytes(32))
-    }
-
-    let hrstart = process.hrtime()
-    let numOfOps = 0
-    let trie = new CheckpointTrie()
-    for (let i = 0; i < numOfIter; i++) {
-      trie.checkpoint()
-      await trie.put(vals[i], keys[i])
-      await trie.get(Buffer.from('test'))
-      numOfOps++
-      if (numOfOps === numOfIter) {
-        const hrend = process.hrtime(hrstart)
-        resolve(hrend)
-      }
-      trie.commit()
-    }
-  })
-}
-
-const go = async () => {
-  let i = 1
-  let avg = [0, 0]
-
-  console.log(`Benchmark 'checkpointing' starting...`)
-  while (i <= samples) {
-    console.log(`Sample ${i} with ${iterations} iterations.`)
-    const hrend = await iterTest(iterations)
-    avg[0] += hrend[0]
-    avg[1] += hrend[1]
-    // console.log('benchmarks/checkpointing.ts | execution time: %ds %dms', hrend[0], (hrend[1] / 1000000).toFixed(3))
-    i++
+  for (let i = 0; i <= numOfIter; i++) {
+    keys.push(pseudoRandomBytes(32))
+    vals.push(pseudoRandomBytes(32))
   }
-  console.log(
-    'benchmarks/checkpointing.ts | average execution time: %ds %dms',
-    avg[0] / samples,
-    (avg[1] / 1000000 / samples).toFixed(3),
-  )
-}
 
-go()
+  const trie = new CheckpointTrie()
+
+  for (let i = 0; i < numOfIter; i++) {
+    trie.checkpoint()
+    await trie.put(keys[i], vals[i])
+    await trie.commit()
+  }
+}
